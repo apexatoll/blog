05ba667e6f2aa61a6fad150134c6608aedfa9709
Christian Welham <welhamm@gmail.com>
2021-09-14 22:01:00 +0100

    implement base controller

--- src/cfg/define.php
+++ src/cfg/define.php
@@ -3,7 +3,9 @@
 define("ROOT", preg_replace("/\/src.*?$/", "", __DIR__));
 define("SRC", ROOT."/src");
 
-define("CTRL_NS", "ctrls");
+define("MODEL_NS", "models");
+define("VIEW_NS",  "views");
+define("CTRL_NS",  "ctrls");
 
 foreach(["css", "js"] as $file)
 	require_once(__DIR__."/$file.php");
