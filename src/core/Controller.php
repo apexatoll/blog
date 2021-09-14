d0e00caa37b97cf8de70c37e24b6f28d387e3d51
Christian Welham <welhamm@gmail.com>
2021-09-14 22:12:33 +0100

    split off controller methods to a trait for reuse in view

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -1,44 +1,13 @@
 <?php namespace core;
 
 class Controller {
+	use traits\ClassParser;
 	public function __construct(){
+		$this->set_classes();
+	}
+	protected function set_classes(){
 		$this->group = $this->get_group_name();
 		$this->m_cls = $this->to_singular();
 		$this->v_cls = $this->group;
-		print_r($this);
-	}
-	private function reflect(){
-		$this->reflect ??= new \ReflectionClass(get_class($this));
-		return $this->reflect;
-	}
-	private function get_group_name(){
-		return $this->is_root_class() ?
-			$this->class_name():
-			$this->namespace_name();
-	}
-	private function is_root_class(){
-		return in_array(
-			$this->reflect()->getNamespaceName(), 
-			[MODEL_NS, VIEW_NS, CTRL_NS]
-		);
-	}
-	private function parse_name($ref_method){
-		return preg_replace(
-			"/^.*?\\\/", "", $this->reflect()->$ref_method()
-		);
-	}
-	private function class_name(){
-		return $this->parse_name("getName");
-	}
-	private function namespace_name(){
-		return ucwords($this->parse_name("getNamespaceName"));
-	}
-	private function unchanging(){
-		return ["Series", "Auth"];
-	}
-	protected function to_singular(){
-		return in_array($this->group, $this->unchanging()) ?
-			$this->group :
-			preg_replace("/ie$/", "y", preg_replace("/s$/", "", $this->group));
 	}
 }
---
+++ src/core/traits/ClassParser.php
@@ -0,0 +1,39 @@
+<?php namespace core\traits;
+
+trait ClassParser {
+	protected abstract function set_classes();
+	private function reflect(){
+		$this->reflect ??= new \ReflectionClass(get_class($this));
+		return $this->reflect;
+	}
+	private function get_group_name(){
+		return $this->is_root_class() ?
+			$this->class_name():
+			$this->namespace_name();
+	}
+	private function is_root_class(){
+		return in_array(
+			$this->reflect()->getNamespaceName(), 
+			[MODEL_NS, VIEW_NS, CTRL_NS]
+		);
+	}
+	private function parse_name($ref_method){
+		return preg_replace(
+			"/^.*?\\\/", "", $this->reflect()->$ref_method()
+		);
+	}
+	private function class_name(){
+		return $this->parse_name("getName");
+	}
+	private function namespace_name(){
+		return ucwords($this->parse_name("getNamespaceName"));
+	}
+	private function unchanging(){
+		return ["Series", "Auth"];
+	}
+	protected function to_singular(){
+		return in_array($this->group, $this->unchanging()) ?
+			$this->group :
+			preg_replace("/ie$/", "y", preg_replace("/s$/", "", $this->group));
+	}
+}
