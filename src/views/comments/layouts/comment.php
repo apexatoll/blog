4a97d49d505858fd9d1c999da005a44c46516666
Christian Welham <welhamm@gmail.com>
2021-09-15 17:10:00 +0100

    comment css

--- src/views/comments/layouts/comment.php
+++ src/views/comments/layouts/comment.php
@@ -1,5 +1,5 @@
 <div class="box comment">
-	<div class="bar top">
+	<div class="bar bar-top bar-no-shadow">
 		<?=$this->user()?>
 		<div class="buttons">
 			<?=$this->reply()?>
@@ -9,7 +9,9 @@
 	</div>
 	<div class="content">
 		<input type="hidden" class="comment-id" value="<?=$this->id?>">
-		<?=$this->date()?>
+		<div class="meta meta-comment">
+			<?=$this->date()?>
+		</div>
 		<div class="body">
 			<?=$this->body?>
 		</div>
--- src/views/comments/layouts/thread.php
+++ src/views/comments/layouts/thread.php
@@ -1,5 +1,4 @@
 <div class="thread">
 	<?=$this->comment($comment)?>
-	<?//=$comment['body']?>
 	<?=$this->thread(["parent"=>$comment['id']]);?>
 </div>
