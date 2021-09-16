f33759536642a31b18a29c1d1ec54294501ff75c
Christian Welham <welhamm@gmail.com>
2021-09-16 16:50:41 +0100

    style edit post images

--- src/views/posts/layouts/image.php
+++ src/views/posts/layouts/image.php
@@ -1,13 +1,13 @@
-<div class="card">
-	<div class="bar top end">
+<div class="image-card">
+	<div class="bar bar-top bar-end bar-no-shadow">
 		<div>
 			<button class="img-add-ref submit"><?=$this->icon("plus");?></button>
 			<button class="star"><?=$this->icon("star");?></button>
 			<button class="img-delete cancel"><?=$this->icon("trash-alt");?></button>
 		</div>
 	</div>
-	<div class="content">
-		<img class="img-popup" alt="<?=$name?>" src="<?=$path?>">
+	<div class="image-card-content">
+		<img class="image-popup" alt="<?=$name?>" src="<?=$path?>">
 		<div class="filename"><?=$name?></div>
 	</div>
 </div>
