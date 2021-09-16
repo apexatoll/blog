cc5eb17960c13721ee2293e017ddb964f48d6feb
Christian Welham <welhamm@gmail.com>
2021-09-16 16:18:43 +0100

    footer tweaks

--- src/views/footers/user.php
+++ src/views/footers/user.php
@@ -1,5 +1,5 @@
 <?=$this->user_span()?>
 <?if($this->session->can_post()):?>
-	<button class="footer-show-post">post</button>
+<button class="footer-show-post">post</button>
 <?endif;?>
 <button class="footer-submit-logout">log out</button>
