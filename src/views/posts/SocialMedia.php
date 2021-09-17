0efbbb8bf2c719c7e82fe4b818ec5fd7c36aef78
Christian Welham <welhamm@gmail.com>
2021-09-17 03:35:53 +0100

    style social media buttons

--- src/views/posts/SocialMedia.php
+++ src/views/posts/SocialMedia.php
@@ -5,16 +5,16 @@ class SocialMedia extends \core\View {
 		return $this->buffer_layout("social");
 	}
 	protected function reddit(){
-		return $this->icon_a("reddit", "");
+		return $this->icon_a("reddit", "", ["class"=>"reddit"]);
 	}
 	protected function facebook(){
-		return $this->icon_a("facebook", "");
+		return $this->icon_a("facebook", "", ["class"=>"facebook"]);
 	}
 	protected function twitter(){
-		return $this->icon_a("twitter", "");
+		return $this->icon_a("twitter", "", ["class"=>"twitter"]);
 	}
 	protected function pinterest(){
-		return $this->icon_a("pinterest", "");
+		return $this->icon_a("pinterest", "", ["class"=>"pinterest"]);
 	}
 	protected function icon_a($icon, $href, $attr=[]){
 		return $this->a($this->span(null, ["class"=>"fab fa-$icon"]), ["class"=>$icon] + $attr);
