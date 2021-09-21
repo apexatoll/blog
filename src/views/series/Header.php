f0e61ef030e682851c21b72979e1d00830489f49
Christian Welham <welhamm@gmail.com>
2021-09-21 01:49:00 +0100

    update series header validation

--- src/views/series/Header.php
+++ src/views/series/Header.php
@@ -2,6 +2,11 @@
 
 class Header extends \views\posts\Header {
 	protected function sort(){
+		if($this->session->is_admin())
 		return $this->icon_a("sort", "/series/sort/$this->id");
+	}
+	protected function edit(){
+		if($this->session->is_admin())
+			return $this->icon_a("edit", "/$this->class/edit/$this->id");
 	}
 }
